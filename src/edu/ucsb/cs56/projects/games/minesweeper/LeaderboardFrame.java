8208bc56322495748896341f6bb877c77ce34e75
Kate Perkins <kateperkins@169-231-153-224.wireless.ucsb.edu>
2017-11-16 10:44:28 -0800

    Leaderboard GUI start

--- src/edu/ucsb/cs56/projects/games/minesweeper/LeaderboardFrame.java
+++ src/edu/ucsb/cs56/projects/games/minesweeper/LeaderboardFrame.java
@@ -1,6 +1,10 @@
 package edu.ucsb.cs56.projects.games.minesweeper;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
+import javax.swing.JTable;
 
 /**
  * Created by ryanwiener on 11/15/17.
@@ -8,5 +12,14 @@ import javax.swing.JFrame;
 
 public class LeaderboardFrame extends JFrame {
 
+    private JLabel title;
+    private JTable table;
+    private JToggleButton diffToggle;
+
+
+    public LeaderboardFrame() {
+
+
+    }
     
 }
